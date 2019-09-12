f0eb7474e3c26c762e70123b3c298f7e4e80811f
Adam Dolinsky <39443561+addocode@users.noreply.github.com>
2019-09-12 12:27:34 +0200

    Erster Upload aller Files
    Das snid alle Files innerhalb des Themes, welche ich für mein Workbook brauche.
    Als Grundlage für meine Website verwende ich das "twentyseventeen"-Theme.

---
+++ 404.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="error-404 not-found">
+				<header style="padding-top: 80px;" class="page-header">
+					
+					<?xml version="1.0" encoding="utf-8"?>
+						<!-- Generator: Adobe Illustrator 23.0.6, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+						<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+							viewBox="0 0 522.3 149" style="enable-background:new 0 0 522.3 149;" xml:space="preserve">
+						<style type="text/css">
+							.st0{fill:#C9C9C9;stroke:#FFFFFF;stroke-width:3;stroke-miterlimit:10;}
+							.st1{fill:#C9C9C9;}
+						</style>
+						<title>404</title>
+						<g>
+							<g id="Ebene_1-2">
+								<rect x="1.5" y="12.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="13.3" y="8.1" class="st0" width="11.8" height="11.8"/>
+								<rect x="25.1" y="5.3" class="st0" width="11.8" height="11.8"/>
+								<rect x="36.9" y="3.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="48.7" y="1.5" class="st0" width="11.8" height="11.8"/>
+								<rect x="60.5" y="1.5" class="st0" width="18.7" height="11.8"/>
+								<rect x="114.6" y="8.1" class="st0" width="11.8" height="11.8"/>
+								<rect x="195.6" y="31.4" class="st0" width="11.8" height="11.8"/>
+								<rect x="91" y="3.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="79.2" y="1.5" class="st0" width="11.8" height="11.8"/>
+								<rect x="1.5" y="24.4" class="st0" width="11.8" height="11.8"/>
+								<rect x="2.2" y="36.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="3.2" y="48" class="st0" width="11.8" height="11.8"/>
+								<rect x="5" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="7.4" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="10.9" y="83.3" class="st0" width="11.8" height="11.8"/>
+								<rect x="16.8" y="95.1" class="st0" width="11.8" height="11.8"/>
+								<rect x="24.4" y="106.9" class="st0" width="11.8" height="11.8"/>
+								<rect x="34.5" y="118.7" class="st0" width="11.8" height="11.8"/>
+								<rect x="46.2" y="128.4" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="135.7" class="st0" width="11.8" height="11.8"/>
+								<rect x="132.9" y="15.5" class="st0" width="11.8" height="11.8"/>
+								<rect x="162.3" y="19.5" class="st0" width="11.8" height="11.8"/>
+								<rect x="146.1" y="36.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="131.1" y="51.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="181.2" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="148.3" y="80.3" class="st0" width="11.8" height="11.8"/>
+								<rect x="121.9" y="89.9" class="st0" width="11.8" height="11.8"/>
+								<rect x="162.7" y="101.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="136" y="119.9" class="st0" width="11.8" height="11.8"/>
+								<rect x="111.3" y="113.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="94.6" y="130.1" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="135.7" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="95.1" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="106.5" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="83.3" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="83.3" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="34.5" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="46.2" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="34.5" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="46.2" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="81.6" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="104.8" y="71.6" class="st0" width="11.8" height="11.8"/>
+								<rect x="81.6" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="93.4" y="59.8" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="48" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="48" class="st0" width="11.8" height="11.8"/>
+								<rect x="58" y="36.2" class="st0" width="11.8" height="11.8"/>
+								<rect x="69.8" y="36.2" class="st0" width="11.8" height="11.8"/>
+								<path class="st1" d="M320,101.2v30.1h-3.6v-30.1h-54.1v-4.4l52.1-81.2h5.5v82.3h19.3v3.3L320,101.2z M316.4,19.5h-0.5l-50.5,78.4
+									h51L316.4,19.5z"/>
+								<path class="st1" d="M392.5,133.2c-7.4,0-13.5-1.9-18.1-5.6c-4.7-3.8-8.5-8.7-10.9-14.3c-2.7-6.1-4.5-12.6-5.4-19.2
+									c-1-6.8-1.5-13.7-1.5-20.6c0-6.9,0.5-13.8,1.5-20.6c0.9-6.7,2.7-13.1,5.4-19.3c2.5-5.6,6.2-10.5,10.9-14.3
+									c4.7-3.8,10.7-5.6,18.1-5.6s13.5,1.9,18.1,5.6c4.7,3.8,8.5,8.7,10.9,14.3c2.7,6.1,4.5,12.6,5.4,19.2c1,6.8,1.5,13.7,1.5,20.6
+									c0,6.9-0.5,13.8-1.5,20.6c-0.9,6.7-2.7,13.1-5.4,19.3c-2.5,5.6-6.2,10.5-10.9,14.3C406,131.3,400,133.2,392.5,133.2z M392.5,16.8
+									c-4.4-0.1-8.8,0.9-12.7,3c-3.5,2-6.5,4.7-8.9,7.9c-2.5,3.3-4.4,7-5.7,10.9c-1.4,4-2.5,8.2-3.3,12.4c-0.7,4-1.2,8.1-1.5,12.2
+									c-0.2,3.9-0.3,7.3-0.3,10.1c0,2.9,0.1,6.3,0.3,10.2c0.2,4.1,0.7,8.1,1.5,12.1c0.8,4.2,1.9,8.4,3.3,12.4c1.3,3.9,3.3,7.6,5.7,10.9
+									c2.4,3.2,5.4,5.9,8.9,7.8c8,4,17.4,4,25.3,0c3.5-2,6.5-4.6,8.9-7.8c2.5-3.3,4.4-7.1,5.7-11c1.4-4,2.5-8.2,3.3-12.4
+									c0.7-4,1.2-8,1.5-12.1c0.2-3.9,0.3-7.3,0.3-10.2s-0.1-6.2-0.3-10.1c-0.2-4.1-0.7-8.1-1.5-12.2c-0.8-4.2-1.9-8.4-3.3-12.4
+									c-1.3-3.9-3.3-7.6-5.7-10.9c-2.4-3.2-5.4-5.9-8.9-7.8C401.3,17.7,397,16.7,392.5,16.8z"/>
+								<path class="st1" d="M503,101.2v30.1h-3.6v-30.1h-54.1v-4.4l52.1-81.2h5.6v82.3h19.3v3.3L503,101.2z M499.4,19.5h-0.5l-50.5,78.4
+									h51L499.4,19.5z"/>
+							</g>
+						</g>
+						</svg>
+					<h1 style="text-align: center; color: #c4c4c4;" class="page-title"><?php _e( 'Diese Seite konnte nicht gefunden werden.', 'twentyseventeen' ); ?></h1>
+				</header><!-- .page-header -->
+				<div class="page-content">
+				    
+					<p style="padding-top: 100px;"><?php _e( 'Die von dir eingegebene URL oder der von dir geklickte Link führte zu einer uns unbekannten Seite. Achte darauf, dass die URL korrekt geschrieben ist. Du kannst auch eine Suche starten, um zur gewünschten oder einer ähnlichen Seite zu gelangen:', 'twentyseventeen' ); ?></p>
+
+					<?php get_search_form(); ?>
+
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
---
+++ search.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap wrap-shadow-search">
+
+	<header class="page-header">
+		<?php if ( have_posts() ) : ?>
+			<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+		<?php else : ?>
+			<h1 style="margin-top: 10px;" class="page-title"><?php _e( 'Keine Ergebnisse gefunden', 'twentyseventeen' ); ?></h1>
+		<?php endif; ?>
+	</header><!-- .page-header -->
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/**
+				 * Run the loop for the search to output the results.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-search.php and that will be used instead.
+				 */
+				get_template_part( 'template-parts/post/content', 'excerpt' );
+
+			endwhile; // End of the loop.
+
+			the_posts_pagination(
+				array(
+					'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+					'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+				)
+			);
+
+		else :
+			?>
+
+			<p style="margin-top: 30px;"><?php _e( 'Es sieht so aus, dass wir nichts mit deinen Suchergebnissen Übereinstimmendes finden konnten. Versuche, deine Suchanfrage entweder einzuschränken, detailierter zu umschreiben oder ein Synonym zu gebrauchen. Du kannst auch probieren, nach etwas anderem zu suchen.', 'twentyseventeen' ); ?></p>
+			<?php
+				
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php
+get_footer();
